3bcb807be8097992ca0dad6417807727450ae295
Soma Nagy <nagysomabalint@gmail.com>
2020-12-27 22:30:09 +0100

    Extended tests

--- test/test_exposed.py
+++ test/test_exposed.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, date, time
 
-from hun_date_parser import datetime2text, text2datetime
+from hun_date_parser import datetime2text, text2datetime, text2date, text2time
 
 
 def test_datetime2text():
@@ -13,7 +13,28 @@ def test_datetime2text():
 
 def test_text2datetime():
     now = datetime(2020, 12, 27)
-    tf = [('ma', [{'start_date': datetime(2020, 12, 27), 'end_date': datetime(2020, 12, 27, 23, 59, 59)}])]
+    tf = [('ma', [{'start_date': datetime(2020, 12, 27), 'end_date': datetime(2020, 12, 27, 23, 59, 59)}]),
+          ('ma reggel', [{'start_date': datetime(2020, 12, 27, 6), 'end_date': datetime(2020, 12, 27, 9, 59, 59)}])]
 
     for inp, out in tf:
         assert text2datetime(inp, now=now) == out
+
+
+def test_text2date():
+    now = datetime(2020, 12, 27)
+    tf = [('ma', [{'start_date': date(2020, 12, 27), 'end_date': date(2020, 12, 27)}]),
+          ('ma reggel', [{'start_date': date(2020, 12, 27), 'end_date': date(2020, 12, 27)}]),
+          ('reggel nyolc óra', [])]
+
+    for inp, out in tf:
+        assert text2date(inp, now=now) == out
+
+
+def test_text2time():
+    now = datetime(2020, 12, 27)
+    tf = [('ma', []),
+          ('ma reggel', [{'start_date': time(6), 'end_date': time(9, 59, 59)}]),
+          ('reggel nyolc óra', [{'start_date': time(8), 'end_date': time(8, 59, 59)}])]
+
+    for inp, out in tf:
+        assert text2time(inp, now=now) == out
