ecd228549e71d5eac9e6df1e9c1993bdc7cda0f4
Soma Nagy <nagysomabalint@gmail.com>
2020-12-27 22:29:59 +0100

    Removed unused import

--- test/test_datetime_extractor.py
+++ test/test_datetime_extractor.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from hun_date_parser.date_parser.datetime_extractor import DatetimeExtractor, assamble_datetime
+from hun_date_parser.date_parser.datetime_extractor import DatetimeExtractor
 
 
 def test_datetime_extractor():
