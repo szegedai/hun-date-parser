5860ac659290595b4a120fb93e3b18f5b040144e
Nagy Soma <nagysomabalint@gmail.com>
2021-10-12 16:23:37 +0200

    Added success flag, docstrings

--- hun_date_parser/date_parser/interval_restriction/__init__.py
+++ hun_date_parser/date_parser/interval_restriction/__init__.py
@@ -1,3 +1,4 @@
-from hun_date_parser.date_parser.interval_restriction.restricted_parsing import extract_within_interval
+from hun_date_parser.date_parser.interval_restriction.restricted_parsing import extract_datetime_within_interval,\
+    ExtractWithinRangeSuccess
 
-__all__ = ['extract_within_interval']
+__all__ = ['extract_datetime_within_interval', 'ExtractWithinRangeSuccess']
