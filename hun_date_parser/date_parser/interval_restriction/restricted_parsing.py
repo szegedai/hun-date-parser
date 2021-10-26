789c52f810d01197d73553d78ebc743e7fd81152
Nagy Soma <nagysomabalint@gmail.com>
2021-10-26 17:47:18 +0200

    Added expect future day param to restricted parsing

--- hun_date_parser/date_parser/interval_restriction/restricted_parsing.py
+++ hun_date_parser/date_parser/interval_restriction/restricted_parsing.py
@@ -37,6 +37,7 @@ def is_relative_datetime(query: str):
 def extract_datetime_within_interval(interval_start: datetime,
                                      interval_end: datetime,
                                      query_text: str,
+                                     expect_future_day: bool = False,
                                      fallback_now=datetime.now()) -> Tuple[ExtractWithinRangeSuccess,
                                                                            List[Dict[str, datelike]]]:
     """
@@ -44,23 +45,32 @@ def extract_datetime_within_interval(interval_start: datetime,
     :param interval_start: Bounding interval start
     :param interval_end: Bounding interval end
     :param query_text: Text to extract datetime interval from
+    :param expect_future_day: Shows if the extracted date will be offset.
     :param fallback_now: When interval restriction is unsuccessful (RELATIVE_TIME_WORD_FALLBACK, OUT_OF_RANGE_FALLBACK)
     the text2datetime function's result is returned, which can be supplied with a pseudo-now datetime
     :return: success flag, restricted time interval
     """
 
-    res = text2datetime(query_text, now=interval_start)
+    res = text2datetime(query_text,
+                        expect_future_day=expect_future_day,
+                        now=interval_start)
 
     for r in res:
         if not (type(r['start_date']) == datetime and type(r['start_date']) == datetime):
-            return ExtractWithinRangeSuccess.OPEN_RANGE_FALLBACK, text2datetime(query_text, now=fallback_now)
+            return ExtractWithinRangeSuccess.OPEN_RANGE_FALLBACK, text2datetime(query_text,
+                                                                                expect_future_day=expect_future_day,
+                                                                                now=fallback_now)
 
         if not (interval_start <= r['start_date'] and r['end_date'] <= interval_end):
             # Extracted datetime is out of expected interval...
-            return ExtractWithinRangeSuccess.OUT_OF_RANGE_FALLBACK, text2datetime(query_text, now=fallback_now)
+            return ExtractWithinRangeSuccess.OUT_OF_RANGE_FALLBACK, text2datetime(query_text,
+                                                                                  expect_future_day=expect_future_day,
+                                                                                  now=fallback_now)
 
     if is_relative_datetime(query_text):
         # Datetime ranges relative to the current timestamp doesn't really make sense in this scenario...
-        return ExtractWithinRangeSuccess.RELATIVE_TIME_WORD_FALLBACK, text2datetime(query_text, now=fallback_now)
+        return ExtractWithinRangeSuccess.RELATIVE_TIME_WORD_FALLBACK, text2datetime(query_text,
+                                                                                    expect_future_day=expect_future_day,
+                                                                                    now=fallback_now)
     else:
         return ExtractWithinRangeSuccess.VALID_IN_RANGE, res
