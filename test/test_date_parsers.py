dd0dff9b29b7c185aa5c88f8d4e0bfa628b0d0d5
Soma Nagy <nagysomabalint@gmail.com>
2023-05-20 18:06:01 +0200

    Refactored tests

--- test/test_date_parsers.py
+++ test/test_date_parsers.py
@@ -25,22 +25,63 @@ tf_named_month = [
     ('április 1', [[Month(4, 'named_month'), Day(1, 'named_month')]]),
     ('legyen jövőre február 12-én', [[Year(2021, 'named_month'), Month(2, 'named_month'), Day(12, 'named_month')]])]
 
+tf_match_relative_day = [
+    ('ma', [[Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(1, 'relative_day')]]),
+    ('ma-holnap', [[Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(1, 'relative_day')],
+                   [Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(2, 'relative_day')]]),
+    ('holnapután reggel nyolc', [[Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(3, 'relative_day')]]),
+    ('legyen ma reggel', [[Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(1, 'relative_day')]]),
+    ('miért nem jöttél tegnap? na majd ma',
+     [[Year(2020, 'relative_day'), Month(10, 'relative_day'), Day(1, 'relative_day')],
+      [Year(2020, 'relative_day'), Month(9, 'relative_day'), Day(30, 'relative_day')]])
+]
 
-def test_match_iso_date():
-    fn = 'match_iso_date'
-    tf = [('2020-01-15', [[Year(2020, fn), Month(1, fn), Day(15, fn)]]),
-          ('legyen 2020-01 elején', [[Year(2020, fn), Month(1, fn)]]),
-          ('2001-ben történt', [[Year(2001, fn)]]),
-          ('2020-12-30-án', [[Year(2020, fn), Month(12, fn), Day(30, fn)]]),
-          ('2020-12-30-án 2020.12.29',
-           [[Year(2020, fn), Month(12, fn), Day(30, fn)], [Year(2020, fn), Month(12, fn), Day(29, fn)]])]
+tf_iso_date = [
+    ('2020-01-15', [[Year(2020, 'match_iso_date'), Month(1, 'match_iso_date'), Day(15, 'match_iso_date')]]),
+    ('legyen 2020-01 elején', [[Year(2020, 'match_iso_date'), Month(1, 'match_iso_date')]]),
+    ('2001-ben történt', [[Year(2001, 'match_iso_date')]]),
+    ('2020-12-30-án', [[Year(2020, 'match_iso_date'), Month(12, 'match_iso_date'), Day(30, 'match_iso_date')]]),
+    ('2020-12-30-án 2020.12.29',
+     [[Year(2020, 'match_iso_date'), Month(12, 'match_iso_date'), Day(30, 'match_iso_date')],
+      [Year(2020, 'match_iso_date'), Month(12, 'match_iso_date'), Day(29, 'match_iso_date')]])]
 
-    for inp, exp in tf:
-        out = match_iso_date(inp)
-        date_parts = []
-        for e in out:
-            date_parts.append(e['date_parts'])
-        assert date_parts == exp
+tf_weekday = [
+    ('múlt vasárnap', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(6, 'weekday')]], False),
+    ('ezen a heten hetfon', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(7, 'weekday')]], False),
+    ('jövő kedden', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(15, 'weekday')]], False),
+    ('előző szombaton ', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(5, 'weekday')]], False),
+    ('miért nem jöttél tegnap? na majd ma', [], False),
+    ('jövő kedden', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(15, 'weekday')]], False),
+    ('szombaton ráérek', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(12, 'weekday')]], True),
+    ('mit szólnál hétfőhöz?', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(14, 'weekday')]], True),
+    ('pénteken ráérek', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(11, 'weekday')]], True),
+    ('múlt hét kedden beszéltünk', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(1, 'weekday')]], True),
+    ('szerdán ráérek', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(16, 'weekday')]], True),
+    ('jövő héten szerdán ráérek', [[Year(2020, 'weekday'), Month(12, 'weekday'), Day(16, 'weekday')]], True)]
+
+tf_week = [('múlthéten', [[Year(2020, "week"), Week(49, "week")]]),
+           ('múlt hét kedden', [[Year(2020, "week"), Week(49, "week")]]),
+           ('ezen a heten hetfon', [[Year(2020, "week"), Week(50, "week")]]),
+           ('jövőhéten', [[Year(2020, "week"), Week(51, "week")]]),
+           ('legyen ma', [])]
+
+tf_named_year = [('tavaly', [[Year(2019, 'named_year')]]),
+                 ('múlt hét kedden', []),
+                 ('legyen meg még idén légyszi', [[Year(2020, 'named_year')]]),
+                 ('kb két év múlva', [[Year(2022, 'named_year')]]),
+                 ('tavalyelőtt történt', [[Year(2018, 'named_year')]]),
+                 ('40 év múlva', [[Year(2060, 'named_year')]]),
+                 ('kb három évvel ezelőtt', [[Year(2017, 'named_year')]]),
+                 ('találkozzunk jövő héten szombaton', [])]
+
+
+@pytest.mark.parametrize("inp,exp", tf_iso_date)
+def test_match_iso_date(inp, exp):
+    out = match_iso_date(inp)
+    date_parts = []
+    for e in out:
+        date_parts.append(e['date_parts'])
+    assert date_parts == exp
 
 
 @pytest.mark.parametrize("inp,exp", tf_named_month)
@@ -54,84 +95,42 @@ def test_named_month(inp, exp):
     assert date_parts == exp
 
 
-def test_match_relative_day():
-    fn = 'relative_day'
+@pytest.mark.parametrize("inp,exp", tf_match_relative_day)
+def test_match_relative_day(inp, exp):
     now = datetime(2020, 10, 1)
 
-    tf = [('ma', [[Year(2020, fn), Month(10, fn), Day(1, fn)]]),
-          ('ma-holnap', [[Year(2020, fn), Month(10, fn), Day(1, fn)], [Year(2020, fn), Month(10, fn), Day(2, fn)]]),
-          ('holnapután reggel nyolc', [[Year(2020, fn), Month(10, fn), Day(3, fn)]]),
-          ('legyen ma reggel', [[Year(2020, fn), Month(10, fn), Day(1, fn)]]),
-          ('miért nem jöttél tegnap? na majd ma',
-           [[Year(2020, fn), Month(10, fn), Day(1, fn)], [Year(2020, fn), Month(9, fn), Day(30, fn)]])]
-
-    for inp, exp in tf:
-        out = match_relative_day(inp, now)
-        date_parts = []
-        for e in out:
-            date_parts.append(e['date_parts'])
-        assert date_parts == exp
+    out = match_relative_day(inp, now)
+    date_parts = []
+    for e in out:
+        date_parts.append(e['date_parts'])
+    assert date_parts == exp
 
 
-def test_match_weekday():
-    fn = 'weekday'
+@pytest.mark.parametrize("inp,exp,_", tf_weekday)
+def test_match_weekday(inp, exp, _):
     now = datetime(2020, 12, 11)  # friday
-
-    tf = [('múlt vasárnap', [[Year(2020, fn), Month(12, fn), Day(6, fn)]], False),
-          ('ezen a heten hetfon', [[Year(2020, fn), Month(12, fn), Day(7, fn)]], False),
-          ('jövő kedden', [[Year(2020, fn), Month(12, fn), Day(15, fn)]], False),
-          ('előző szombaton ', [[Year(2020, fn), Month(12, fn), Day(5, fn)]], False),
-          ('miért nem jöttél tegnap? na majd ma', [], False),
-          ('jövő kedden', [[Year(2020, fn), Month(12, fn), Day(15, fn)]], False),
-          ('szombaton ráérek', [[Year(2020, fn), Month(12, fn), Day(12, fn)]], True),
-          ('mit szólnál hétfőhöz?', [[Year(2020, fn), Month(12, fn), Day(14, fn)]], True),
-          ('pénteken ráérek', [[Year(2020, fn), Month(12, fn), Day(11, fn)]], True),
-          ('múlt hét kedden beszéltünk', [[Year(2020, fn), Month(12, fn), Day(1, fn)]], True),
-          ('szerdán ráérek', [[Year(2020, fn), Month(12, fn), Day(16, fn)]], True),
-          ('jövő héten szerdán ráérek', [[Year(2020, fn), Month(12, fn), Day(16, fn)]], True)]
-
-    for inp, exp, expect in tf:
-        out = match_weekday(inp, now, expect)
-        date_parts = []
-        for e in out:
-            date_parts.append(e['date_parts'])
-        assert date_parts == exp
+    out = match_weekday(inp, now, exp)
+    date_parts = []
+    for e in out:
+        date_parts.append(e['date_parts'])
+    assert date_parts == exp
 
 
-def test_match_match_week():
-    fn = 'week'
+@pytest.mark.parametrize("inp,exp", tf_week)
+def test_match_match_week(inp, exp):
     now = datetime(2020, 12, 7)
-
-    tf = [('múlthéten', [[Year(2020, fn), Week(49, fn)]]),
-          ('múlt hét kedden', [[Year(2020, fn), Week(49, fn)]]),
-          ('ezen a heten hetfon', [[Year(2020, fn), Week(50, fn)]]),
-          ('jövőhéten', [[Year(2020, fn), Week(51, fn)]]),
-          ('legyen ma', [])]
-
-    for inp, exp in tf:
-        out = match_week(inp, now)
-        date_parts = []
-        for e in out:
-            date_parts.append(e['date_parts'])
-        assert date_parts == exp
+    out = match_week(inp, now)
+    date_parts = []
+    for e in out:
+        date_parts.append(e['date_parts'])
+    assert date_parts == exp
 
 
-def test_match_named_year():
-    fn = 'named_year'
+@pytest.mark.parametrize("inp,exp", tf_named_year)
+def test_match_named_year(inp, exp):
     now = datetime(2020, 12, 7)
-
-    tf = [('tavaly', [[Year(2019, fn)]]),
-          ('múlt hét kedden', []),
-          ('legyen meg még idén légyszi', [[Year(2020, fn)]]),
-          ('kb két év múlva', [[Year(2022, fn)]]),
-          ('tavalyelőtt történt', [[Year(2018, fn)]]),
-          ('40 év múlva', [[Year(2060, fn)]]),
-          ('kb három évvel ezelőtt', [[Year(2017, fn)]]),
-          ('találkozzunk jövő héten szombaton', [])]
-
-    for inp, exp in tf:
-        out = match_named_year(inp, now)
-        date_parts = []
-        for e in out:
-            date_parts.append(e['date_parts'])
-        assert date_parts == exp
+    out = match_named_year(inp, now)
+    date_parts = []
+    for e in out:
+        date_parts.append(e['date_parts'])
+    assert date_parts == exp
