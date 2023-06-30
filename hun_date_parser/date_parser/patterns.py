16093ff24358042d75bc7e056a749dab01f4c743
Soma Nagy <nagysomabalint@gmail.com>
2023-06-30 14:20:31 +0200

    Extended ISO pattern

--- hun_date_parser/date_parser/patterns.py
+++ hun_date_parser/date_parser/patterns.py
@@ -7,8 +7,8 @@ R_TOL = r'([:\w ]*-?t[oóöő]l\b).*'
 R_IG = r'([:\w ]*-?ig\b)'
 
 # hyper day level patterns
-R_ISO_DATE = r'(\b\d{4,4})(?:[-\\/\. ](1[0-2]|0?[1-9]))?(?:[-\\/\. ](1[0-9]|2[0-9]|3[01]|0?[1-9]))?'
-R_REV_ISO_DATE = r'\b(1[0-9]|2[0-9]|3[01]|0?[1-9])[-\\/\. ](1[0-2]|0?[1-9])[-\\/\. ](\b\d{4,4})'
+R_ISO_DATE = r'(\b\d{4,4})(?:[-\\/\. ] ?(1[0-2]|0?[1-9]))?(?:[-\\/\. ] ?(1[0-9]|2[0-9]|3[01]|0?[1-9]))?'
+R_REV_ISO_DATE = r'\b(1[0-9]|2[0-9]|3[01]|0?[1-9])[-\\/\. ] ?(1[0-2]|0?[1-9])[-\\/\. ] ?(\b\d{4,4})'
 R_NAMED_MONTH = r'(j[oöő]v[oöő].*?|tavaly.*?)?(\bjan(?:\b|\.|u[aá]r){1}|\bfeb(?:\b|r\.|\.|ru[aá]r){1}|\bm[aá]r(?:\b|c\b|c\.|\.|cius){1}|\b[aá]pr(?:\b|\.|ilis){1}\b|m[aá]j(?:\b|\.|us){1}|\bj[uú]n(?:\b|\.|ius){1}|\bj[uú]l(?:\b|\.|ius){1}|\baug(?:\b|\.|usztus){1}|\bszep(?:t\b|t\.|\b|\.|tember){1}|\bokt(?:\b|\.|[oó]ber){1}|\bnov(?:\b|\.|ember){1}|\bdec(?:\b|\.|ember){1})(?: ([1-3][0-9]|[1-9]))?'
 R_RELATIVE_MONTH = r'(?:(\blegut[oó]bbi|\butols[oó]|\bmúlt|\but[oó]bbi|\bezen|\bebben|\baktu[aá]lis|\bj[oöő]v[oöő]|\bk[oö]vetkez[oőö]|\bk[oö]vetkezend[oőö]).*)? a?h[oó]nap'
 
